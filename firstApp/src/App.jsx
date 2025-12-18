c220a32cb6874283a649b517f32da9cebdd32201
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-19 02:30:19 +0530

    updated css

--- firstApp/src/App.jsx
+++ firstApp/src/App.jsx
@@ -4,11 +4,17 @@ import Cube from "./Cube";
 
 const App = () => {
   return (
-    <Canvas camera={{fov:65}}>
+    <>
+      <h1>3D Image Carousel Cylinder</h1>
+      <p>My first React Three Fiber project.I wrapped a PNG texture on a rotating 3D cylinder.</p>
+
+      <Canvas camera={{fov:65}}>
        <OrbitControls/>
        <ambientLight/>
        <Cube/>
     </Canvas>
+    </>
+    
   )
 };
 
