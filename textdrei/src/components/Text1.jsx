318f663fac214efbad2fd8e2f2b95948cafb7f1b
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-26 19:04:12 +0530

    added MeshReflectorMaterial

--- textdrei/src/components/Text1.jsx
+++ textdrei/src/components/Text1.jsx
@@ -1,8 +1,9 @@
-import { Center, Float, Text3D } from "@react-three/drei";
+import { Center, Float, MeshReflectorMaterial, MeshWobbleMaterial, Text3D } from "@react-three/drei";
 
 const Text1 = () => {
   return (
-<Center>
+  <>
+   <Center>
   <Float 
      speed={5}        // move speed
      rotationIntensity={1.5} // rotate amount
@@ -21,26 +22,24 @@ const Text1 = () => {
   <meshStandardMaterial color="yellow" />
 </Text3D>
 </Float>
-<Float 
-     speed={5}        // move speed
-     rotationIntensity={1.5} // rotate amount
-     floatIntensity={2}      // up-down move
-  >
-<Text3D
-  font="/fonts/helvetiker_bold.typeface.json"
-  size={3.5}
-  height={0.3}
-  bevelEnabled
-  bevelSize={0.02}
-  bevelThickness={0.02}
-  position={[0, -2, 0]} // move second text down
->
-  Hello 3D
-  <meshStandardMaterial color="skyblue" />
-</Text3D>
-
-  </Float>
 </Center>
+
+  {/* Reflective floor */}
+      <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
+        <planeGeometry args={[20, 20]} />
+        <MeshReflectorMaterial
+          blur={[400, 100]}      // blur intensity
+          resolution={1024}      // texture resolution
+          mixStrength={0.5}      // reflection strength
+          color="greenyellow"
+          metalness={0.5}
+          roughness={1}
+          mirror={0.75}
+          // mixBlur={5}
+        />
+      </mesh>
+     
+</>
 
   );
 };
