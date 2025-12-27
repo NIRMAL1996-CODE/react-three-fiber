c98afef521e6c10a6d66e17a12157b2f70628208
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-27 22:58:57 +0530

    Stage Helper in React Three Fiber

--- bgEnvironment/src/Box.jsx
+++ bgEnvironment/src/Box.jsx
@@ -24,7 +24,7 @@ const Box = () => {
 
       {/*lightformer  */}
 
-       <Environment background  preset="forest" > 
+       {/* <Environment background  preset="forest" > 
      
         <Lightformer intensity={5}
         scale={10} 
@@ -32,16 +32,31 @@ const Box = () => {
         position={[0, 0, -5]}
         form="ring" />
 
-      </Environment>
-    
-    <mesh position={[2, 1, 0]}>
+      </Environment> */}
+       {/* <mesh position={[2, 1, 0]}>
       <boxGeometry />
       <meshStandardMaterial  />
     </mesh>
      <mesh position={[-2, 1, 0]}>
       <sphereGeometry />
       <meshStandardMaterial  />
+    </mesh> */}
+
+    {/* Stage Helper in React Three Fiber */}
+    <Stage contactShadow={{opacity : 0.2, blur : 3}}
+    environment="sunset"
+    preset="portrait"
+    intensity={2}>
+       <mesh position={[2, 1, 0]}>
+      <boxGeometry />
+      <meshStandardMaterial color="mediumpurple" />
     </mesh>
+     <mesh position={[-2, 1, 0]}>
+      <sphereGeometry />
+      <meshStandardMaterial  color="yellow"/>
+    </mesh>
+    </Stage>
+    
     </>
   );
 };
