c89058cad8f6c5bf7c4ec3fc617c5e3ac294581b
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-27 22:40:25 +0530

    Custom Environment with Background and Plane

--- bgEnvironment/src/Box.jsx
+++ bgEnvironment/src/Box.jsx
@@ -4,12 +4,24 @@ const Box = () => {
   return (
     <>
     {/* Environment Mapping */}
-     <Environment 
+     {/* <Environment 
       // background 
       ground={{ height: 5, radius: 28, scale: 100 }}
-     preset="forest" />
+     preset="forest" /> */}
      {/* sunset, city, forest, studio, warehouse, night */}
 
+    {/* Custom Environment with Background and Plane */}
+     <Environment  background >
+        <color args={["black"]} 
+        attach= "background" />
+
+          <mesh position-z={-5} scale={10}>
+          <planeGeometry />
+          <meshBasicMaterial color={[10,0,0]} />
+          </mesh>
+          
+      </Environment>
+    
     <mesh position={[2, 1, 0]}>
       <boxGeometry />
       <meshStandardMaterial  />
