2a97507813b7cd1d5d8553ea0f1d93903804c893
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-12-31 02:34:20 +0530

    model animations

--- ModelLoader/src/Model.jsx
+++ ModelLoader/src/Model.jsx
@@ -1,9 +1,26 @@
-import { useGLTF } from "@react-three/drei";
+// import { useGLTF } from "@react-three/drei";
+
+// const Model = () => {
+//   const model = useGLTF("/car.glb");
+
+//   return <primitive object={model.scene} scale={0.9} position-y={-0.2}/>;
+// };
+
+// export default Model;
+
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { useEffect } from "react";
 
 const Model = () => {
-  const model = useGLTF("/car.glb");
+  const gltf = useGLTF("/hand.glb");
+  const { actions } = useAnimations(gltf.animations, gltf.scene);
 
-  return <primitive object={model.scene} scale={0.9} position-y={-0.2}/>;
+  // actions["AnimationName"]?.play(); // start animation
+useEffect(() => {
+    const names = Object.keys(actions);
+    console.log("Available animations:", names);
+    if (names[0]) actions[names[0]].play();
+  }, [actions]);
+  return <primitive object={gltf.scene} scale={0.9}/>;
 };
-
 export default Model;
